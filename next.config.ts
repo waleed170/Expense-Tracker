3cb9bb633fab17ee8cf15ccd69a9f146a5747069
Muhammad Waleed Monim <126187564+waleed170@users.noreply.github.com>
2025-09-13 11:19:24 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* other config options can stay here if you add later */
 };
 
 export default nextConfig;
